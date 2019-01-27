58ac5fe48796c23316814399d48bf4894c3ff9a4
crawlersick <johnbyyang@gmail.com>
2019-01-27 14:36:57 +0800

    mm

--- static/def_js/1.js
+++ static/def_js/1.js
@@ -1,1 +1,49 @@
 console.log('--------js log start-------------------')
+var files
+$('input[type=file]').change(function(e){
+fn=e.target.files[0].name
+$($(this).next()[0]).text(fn)
+files = event.target.files
+})
+
+$('#sbt1').click(sbt1f)
+
+function sbt1f(){console.log('1111111111111')
+
+
+    var data = new FormData();
+    $.each(files, function(key, value)
+    {
+        data.append(key, value);
+    });
+
+ $.ajax({
+        url: 'submit.php?files',
+        type: 'POST',
+        data: data,
+        cache: false,
+        dataType: 'json',
+        processData: false, // Don't process the files
+        contentType: false, // Set content type to false as jQuery will tell the server its a query string request
+        success: function(data, textStatus, jqXHR)
+        {
+            if(typeof data.error === 'undefined')
+            {
+                // Success so call function to process the form
+                submitForm(event, data);
+            }
+            else
+            {
+                // Handle errors here
+                console.log('ERRORS: ' + data.error);
+            }
+        },
+        error: function(jqXHR, textStatus, errorThrown)
+        {
+            // Handle errors here
+            console.log('ERRORS: ' + textStatus);
+            // STOP LOADING SPINNER
+        }
+    })
+
+}
--- web_inf.py
+++ web_inf.py
@@ -1,5 +1,4 @@
-
-from flask import Flask,render_template,url_for
+from flask import Flask, render_template, url_for
 from flask import jsonify
 import os
 from flask_cors import CORS
@@ -8,11 +7,13 @@ app = Flask(__name__)
 CORS(app)
 app.config['JSON_AS_ASCII'] = False
 
+
 @app.route('/')
 def hello_world():
-    #return 'Hello, World!'
+    # return 'Hello, World!'
     return render_template('home.html')
+
 
 if __name__ == '__main__':
     print("start...")
-    app.run(host='0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
