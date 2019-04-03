ef60f1184ba90143b4b40eefa3f5c5e9308ccc7d
crawlersick <johnbyyang@gmail.com>
2019-04-03 15:15:41 +0800

    ,

--- app_batchwatcher.py
+++ app_batchwatcher.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 from app.batchwatcher import a1
 app = Flask(__name__)
+app.config.update(RESTFUL_JSON=dict(ensure_ascii=False))
 api = Api(app)
 api.add_resource(a1.HelloWorld,'/')
 if __name__ == '__main__':
