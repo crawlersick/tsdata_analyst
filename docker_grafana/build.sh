c275f895c8e694513e3c36197cb86da94550e041
crawlersick <johnbyyang@gmail.com>
2019-06-12 11:06:59 +0800

    add plugins

--- docker_grafana/build.sh
+++ docker_grafana/build.sh
@@ -1,5 +1,5 @@
 docker build -t grafana:latest-with-plugins \
-	 --build-arg http_proxy=192.168.8.100:8998 \
-	 --build-arg https_proxy=192.168.8.100:8998 \
+	 --build-arg http_proxy=http://192.168.8.100:8998 \
+	 --build-arg https_proxy=http://192.168.8.100:8998 \
   --build-arg "GRAFANA_VERSION=latest" \
-  --build-arg "GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-simple-json-datasource,grafana-piechart-panel,natel-plotly-panel,ryantxu-ajax-panel,briangann-datatable-panel,jdbranham-diagram-panel,alexanderzobnin-zabbix-app,neocat-cal-heatmap-panel" .
+  --build-arg "GF_INSTALL_PLUGINS=grafana-clock-panel,grafana-simple-json-datasource,grafana-piechart-panel,natel-plotly-panel,ryantxu-ajax-panel,briangann-datatable-panel,jdbranham-diagram-panel,alexanderzobnin-zabbix-app,neocat-cal-heatmap-panel,vertamedia-clickhouse-datasource" .
